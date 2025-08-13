c736923d0bb63cabec66e7f043cc5d257f5efeea
joseph9n6 <joseph9n6kaka@live.no>
2025-08-13 21:35:14 +0200

    Update GameWorld.java

--- Spill prosjekt laget av Java/GameWorld.java
+++ Spill prosjekt laget av Java/GameWorld.java
@@ -29,7 +29,7 @@ public class GameWorld extends JPanel implements ActionListener, KeyListener {
 
     private BufferedImage[][] animasjoner = new BufferedImage[4][3];
     private BufferedImage spillerStille;
-    private BufferedImage nullBilde;
+    private BufferedImage veiBilde;
     private BufferedImage gressBilde;
     private BufferedImage byggningBilde;
     private BufferedImage traeBilde;
@@ -42,6 +42,14 @@ public class GameWorld extends JPanel implements ActionListener, KeyListener {
     private BufferedImage veiBilToBilde;
     private BufferedImage jordBilde;
     private BufferedImage jordBilVenstreBilde;
+    private BufferedImage jordBilVenstreToBilde;
+    private BufferedImage jordBilHoyreBilde;
+    private BufferedImage jordBilHoyreToBilde;
+    private BufferedImage hotelBilde;
+    private BufferedImage bilHotelBilde;
+    private BufferedImage bilHotelToBilde;
+    private BufferedImage bilHotelTreBilde;
+    private BufferedImage bilHotelFireBilde;
 
     private int retning = 0;
     private int animasjonsFrame = 0;
@@ -80,7 +88,8 @@ public class GameWorld extends JPanel implements ActionListener, KeyListener {
             BufferedImage hoyre = ImageIO.read(new File("sprites/spillerHøyre.png"));
             BufferedImage opp = ImageIO.read(new File("sprites/spillerOpp.png"));
 
-            nullBilde = ImageIO.read(new File("sprites/0=0.png"));
+            // Ting med bygninger å gjøre? 
+            veiBilde = ImageIO.read(new File("sprites/vei.png"));
             gressBilde = ImageIO.read(new File("sprites/gress.png"));
             byggningBilde = ImageIO.read(new File("sprites/byggning.png"));
             traeBilde = ImageIO.read(new File("sprites/trær.png"));
@@ -89,10 +98,21 @@ public class GameWorld extends JPanel implements ActionListener, KeyListener {
             dørBilde = ImageIO.read(new File("sprites/dør.png"));
             dørToBilde = ImageIO.read(new File("sprites/dørto.png"));
             byggningStiBilde = ImageIO.read(new File("sprites/byggningsti.png"));
-            veiBilBilde = ImageIO.read(new File("sprites/veiBil.png"));
-            veiBilToBilde = ImageIO.read(new File("sprites/veiBilTo.png"));
+            hotelBilde = ImageIO.read(new File("sprites/hotel.png"));
+            bilHotelBilde = ImageIO.read(new File("sprites/bilHotel.png"));
+            bilHotelToBilde = ImageIO.read(new File("sprites/bilHotelTo.png"));
+            bilHotelTreBilde = ImageIO.read(new File("sprites/bilHotelTre.png"));
+            bilHotelFireBilde = ImageIO.read(new File("sprites/bilHotelFire.png"));
+
+            // 🚨 Oppdatert her – bruk riktige filnavn:
+            veiBilBilde = ImageIO.read(new File("sprites/bilVei.png"));
+            veiBilToBilde = ImageIO.read(new File("sprites/bilVeiTo.png"));
+
             jordBilde = ImageIO.read(new File("sprites/jord.png"));
             jordBilVenstreBilde = ImageIO.read(new File("sprites/jordbilvenstre.png"));
+            jordBilVenstreToBilde = ImageIO.read(new File("sprites/jordbilvenstreto.png"));
+            jordBilHoyreBilde = ImageIO.read(new File("sprites/jordbilhoyre.png"));
+            jordBilHoyreToBilde = ImageIO.read(new File("sprites/jordbilhoyreto.png"));
 
             for (int i = 0; i < 3; i++) {
                 animasjoner[0][i] = ned.getSubimage(i * SPRITE_SIZE, 0, SPRITE_SIZE, SPRITE_SIZE);
@@ -114,7 +134,7 @@ public class GameWorld extends JPanel implements ActionListener, KeyListener {
         for (int y = 0; y < kart.length; y++) {
             for (int x = 0; x < kart[0].length; x++) {
                 switch (kart[y][x]) {
-                    case 0 -> g.drawImage(nullBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                    case 0 -> g.drawImage(veiBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
                     case 1 -> g.drawImage(byggningBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
                     case 2 -> g.drawImage(gressBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
                     case 3 -> g.drawImage(traeBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
@@ -126,7 +146,16 @@ public class GameWorld extends JPanel implements ActionListener, KeyListener {
                     case 9 -> g.drawImage(veiBilBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
                     case 10 -> g.drawImage(veiBilToBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
                     case 11 -> g.drawImage(jordBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
-                    case 13 -> g.drawImage(jordBilVenstreBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                    case 12 -> g.drawImage(jordBilVenstreBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                    case 13 -> g.drawImage(jordBilVenstreToBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                    case 14 -> g.drawImage(jordBilHoyreBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                    case 15 -> g.drawImage(jordBilHoyreToBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                    case 16 -> g.drawImage(hotelBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                    case 17 -> g.drawImage(bilHotelBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                    case 18 -> g.drawImage(bilHotelToBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                    case 19 -> g.drawImage(bilHotelTreBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                    case 20 -> g.drawImage(bilHotelFireBilde, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+
                     default -> {
                         g.setColor(Color.MAGENTA);
                         g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
@@ -229,4 +258,3 @@ public class GameWorld extends JPanel implements ActionListener, KeyListener {
         frame.setVisible(true);
     }
 }
-
